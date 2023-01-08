79f697c4cb2ae75b85683fd6208d5769f91f223c
CalvinBauman <calvin.bauman@icloud.com>
2023-01-07 18:16:02 -0800

    updating schema with database

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,34 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
 CREATE TABLE department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY,
-    title VARCHAR(30)
-    salary DECIMAL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(12,2) NOT NULL,
     department_id INT,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employees(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
     manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
 );
