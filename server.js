ea4a47e068fd664c7863bf7fe9b8720855c725d3
CalvinBauman <calvin.bauman@icloud.com>
2023-01-07 18:52:26 -0800

    adding employee choices

--- server.js
+++ server.js
@@ -19,3 +19,38 @@ const db = mysql.createConnection(
     database: 'employeetracker_db'
   },
 );
+
+function question(){
+
+    let employeeChoices = [];
+        db.query('SELECT first_name, last_name FROM employees', function (err, results) {
+                if (err) throw err;
+                for (let i = 0; i < results.length; i++) {
+                employeeChoices.push(results[i].first_name + " " + results[i].last_name);
+                } 
+              });
+    let roleChoices = [];
+            db.query('SELECT title FROM role', function (err, results) {
+                if (err) throw err;
+                for (let i = 0; i < results.length; i++) {
+                roleChoices.push(results[i].title);
+                } 
+              });
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices:[
+               "View All Employees",
+               "Add Employee",
+               "Update Employee Role",
+               "View All Roles",
+               "Add Role",
+               "View All Departments",
+               "Add Department",
+               "Quit"
+            ]
+        }
+    ])
