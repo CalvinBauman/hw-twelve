420732fd526650c90348ca1743d69917057ceebb
CalvinBauman <calvin.bauman@icloud.com>
2023-01-07 18:54:11 -0800

    adding employee info prompts

--- server.js
+++ server.js
@@ -54,3 +54,96 @@ function question(){
             ]
         }
     ])
+    .then((ans) => {
+        if(ans.choice === "View All Employees"){
+            db.query('SELECT employees.id,employees.first_name,employees.last_name, role.title, role.salary, employees.manager_id FROM Employees INNER JOIN role ON employees.role_id = role.id', function (err, results) {
+                console.table(results);
+                question();
+              });
+        }
+        else if (ans.choice === "Add Employee"){
+            inquirer.prompt([
+                {
+                    type: "text",
+                    name: "first_name",
+                    message:"What is the employees first name?"
+                },
+                {
+                    type: 'text',
+                    name: 'last_name',
+                    message: "What is the employees last name?"
+                },
+                {
+                    type: 'list',
+                    name: 'role_id',
+                    message: "What is the employees role?",
+                    choices:[
+                    {
+                        name: "Sales Lead",
+                        value: 1
+                    },
+                    {
+                        name: "Sales Person",
+                        value: 2
+                    },
+                    {
+                        name: "Lead Engineer",
+                        value: 3
+                    },          
+                    {
+                        name: "Software Engineer",
+                        value: 4
+                    },
+                    {
+                        name: "Account Manager",
+                        value: 5
+                    },
+                    {
+                        name: "Accountant",
+                        value: 6
+                    },
+                    {
+                        name: "Legal Team Lead",
+                        value: 7
+                    },
+                    {
+                        name: "Lawyer",
+                        value: 8
+                    }
+                    ]
+                },
+                {
+                    type: 'list',
+                    name:'manager_id',
+                    message: "Who is the employee's manager",
+                    choices: [
+                        {   
+                            name: "none",
+                            value: null
+                        },
+                        {
+                            name: "John Doe",
+                            value: 1
+                        },
+                        {
+                            name: "Ashley Rodriguez",
+                            value: 3
+                        },
+                        {
+                            name: "Kunal Singh",
+                            value: 5
+                        },
+                        {
+                            name:"Sarah Lourd",
+                            value: 7
+                        }
+                    ]
+                }
+            ]).then((ans) => {
+                db.query('INSERT INTO employees (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)',[ans.first_name,ans.last_name,ans.role_id, ans.manager_id], function (err, results) {
+                    if (err) throw err;
+                    console.log("Employee Added!");
+                    question();
+                  });
+            });
+        }
