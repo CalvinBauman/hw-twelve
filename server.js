1979ab6bdd30214f419c84a0e2523a7906252012
CalvinBauman <calvin.bauman@icloud.com>
2023-01-07 18:36:37 -0800

    server and connect db

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-//starter file
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // Add MySQL password here
+    password: 'password',
+    database: 'employeetracker_db'
+  },
+);
